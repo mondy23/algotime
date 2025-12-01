353828dd25711abcfec37504667f632690441fa4
mondy23 <149188691+mondy23@users.noreply.github.com>
2025-12-01 21:35:56 +0800

    Configure output, basePath, and assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/algotime",
+  assetPrefix: "/algotime/",
 };
 
 export default nextConfig;
